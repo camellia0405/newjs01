84dc427d6db6c919d140e741f33c4f7dbd5e2ec5
umehara2jp <u.natsu.co@gmail.com>
2021-06-12 21:05:27 +0900

    [回数表示を訂正]

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@
 for(let step = 1 ; step<=100; step++){
 
     if (step % 3==0 && step % 5 ==0){
-        console.log(step+"アホ犬になる")
+        console.log(step+"回目　アホ犬になる")
     }else if(step % 3 == 0){
-        console.log(step+"アホになる")
+        console.log(step+"回目　アホになる")
     }else if(step % 5 == 0){
-        console.log(step+"犬になる")
+        console.log(step+"回目　犬になる")
     }else{
-        console.log(step)
+        console.log(step+"回目")
     }
 }
