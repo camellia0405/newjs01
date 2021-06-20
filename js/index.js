2463c36a45d27cddd23fe007d704ff2a98c7ab36
umehara2jp <u.natsu.co@gmail.com>
2021-06-20 12:02:46 +0900

    [チャレンジ追加酒精]

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@
 
 for(let step = 1 ; step<=100; step++){
 
-    if ((step % 2)!=0 && step % 3==0 && step % 5 ==0){
+    if ((step % 2)==0 && step % 3==0 && step % 5 ==0){
         console.log(step+"回目　アホ犬になる")
-    }else if((step % 2)!=0 && step % 3 == 0){
+    }else if((step % 2)==0 && step % 3 == 0){
         console.log(step+"回目　アホになる")
-    }else if((step % 2)!=0 && step % 5 == 0){
+    }else if((step % 2)==0 && step % 5 == 0){
         console.log(step+"回目　犬になる")
-    }else if((step % 2)!=0){
+    }else if((step % 2)==0){
         console.log(step+"回目")
     }
 }
